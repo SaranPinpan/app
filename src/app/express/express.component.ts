c511f5887c412e95238b3c1a8b7566331dbc2cd7
Saran Pinpan <5606021630217@fitm.kmutnb.ac.th>
2020-10-01 13:49:30 +0700

    fix img

--- src/app/express/express.component.ts
+++ src/app/express/express.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var require: any
+
 @Component({
   selector: 'app-express',
   templateUrl: './express.component.html',
   styleUrls: ['./express.component.css']
 })
 export class ExpressComponent implements OnInit {
+
+  img = '../../assets/img.png'
 
   constructor() { }
 
