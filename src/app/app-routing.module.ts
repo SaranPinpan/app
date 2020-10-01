96cb73ed90ce42a49e4f2d01d73ca67a13047aa7
Saran Pinpan <5606021630217@fitm.kmutnb.ac.th>
2020-10-01 14:48:23 +0700

    Temporary change index page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { FirstComponent } from './first/first.component'
-import { SecondComponent } from './second/second.component'
 import { HomeComponent } from './home/home.component'
 import { NodeComponent } from './node/node.component'
 import { AngularComponent } from './angular/angular.component'
 import { ExpressComponent } from './express/express.component'
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'first-component', component: FirstComponent},
-  {path: 'second-component', component: SecondComponent},
+  // {path: '', component: HomeComponent},
   {path: 'nodejs', component: NodeComponent},
   {path: 'angular', component: AngularComponent},
-  {path: 'express', component: ExpressComponent}
+  {path: 'express', component: ExpressComponent},
+  {path: '', redirectTo: '/nodejs', pathMatch:'full'} //Temporary index
 ];
 
 @NgModule({
